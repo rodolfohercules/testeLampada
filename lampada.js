c5e7640f6f0e7948c397ba3a672604517bab8f42
Rodolfo Hercules <rodolfohercules@gmail.com>
2021-04-12 11:15:21 -0300

    second commit

--- lampada.js
+++ lampada.js
@@ -1,5 +1,5 @@
-const turnOn = document.getElementById ( 'turnOn' );
-const turnOff =  document.getElementById ( 'turnOff' );
+const turnOnOff = document.getElementById ( 'turnOnOff' );
+
 const lamp = document.getElementById ( 'lamp' );
 
 function isLampBroken () {
@@ -8,23 +8,33 @@ function isLampBroken () {
 
 function lampOn () {
     if ( !isLampBroken () ) {
-     lamp.src = '../img/ligada.jpg';
+        lamp.src = './img/ligada.jpg';
     }
 }
 
 function lampOff () {
     if ( !isLampBroken () ) {
-    lamp.src = '../img/desligada.jpg';
-       }
-    
+        lamp.src = './img/desligada.jpg';
+    }
 }
 
 function lampBroken () {
-    lamp.src = '../img/quebrada.jpg'
+    lamp.src = './img/quebrada.jpg';
 }
 
-turnOn.addEventListener ( 'click', lampOn );
-turnOff.addEventListener ( 'click', lampOff );
+function lampOnOff () {
+    if ( turnOnOff.textContent == 'Ligar' ) {
+        lampOn();
+        turnOnOff.textContent = 'Desligar';
+    }else{
+        lampOff();
+        turnOnOff.textContent = 'Ligar';
+    }
+}
+
+
+turnOnOff.addEventListener ( 'click', lampOnOff );
+
 lamp.addEventListener ( 'mouseover', lampOn );
-lamp.addEventListener ( 'mouseleave', lampOff ); 
+lamp.addEventListener ( 'mouseleave', lampOff );
 lamp.addEventListener ( 'dblclick', lampBroken );
